7869d2f2e258a83065dc270518769bcfa07976b4
tsujiyudai <tsuji.yudai.xv@tut.jp>
2026-02-01 14:39:32 +0900

    a

--- test_cases.py
+++ test_cases.py
@@ -5,6 +5,7 @@ from calc_mul import calc
 
 # Run with testrunner so needs to be in file test_
 
+
 class TestCalc (unittest.TestCase):
 
         def test_sample1 (self):
@@ -18,4 +19,20 @@ class TestCalc (unittest.TestCase):
 
         def test_sample4 (self):
                 self.assertEqual (-1, calc(0.1,999))
+        
+        def test1 (self):
+                self.assertEqual (200, calc(20 ,10))
+
+        def test2 (self):
+                self.assertEqual (1, calc(1 ,1))
+
+        def test3 (self):
+                self.assertEqual (998001, calc(999 ,999))
+
+        def test4 (self):
+                self.assertEqual (-1, calc(0 ,1000))
+
+        def test5 (self):
+                self.assertEqual (-1, calc(10 ,"abc"))
+        
 
