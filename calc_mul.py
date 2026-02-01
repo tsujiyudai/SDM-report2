4855a12206030c8356b35e77f441f28d71a3be4e
tsujiyudai <tsuji.yudai.xv@tut.jp>
2026-02-01 15:00:44 +0900

    shuusei

--- calc_mul.py
+++ calc_mul.py
@@ -5,11 +5,15 @@ import re
 def calc(A,B):
         ai=str(A)
         bi=str(B)
+
+        if ai.isdigit() == False or bi.isdigit() == False:
+                return -1
+
         p = re.compile('\d+(\.\d+)?')
         if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
+                a=int(ai)
+                b=int(bi)
+                if 0<a and a<1000  and 0<b and b<1000:
                         valid=True
                 else:
                         valid=False
